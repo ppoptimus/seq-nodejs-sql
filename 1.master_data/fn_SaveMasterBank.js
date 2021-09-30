eda33a313d21b6e8fee05633a22219ef5030c830
Rathapong <dev01tcm@outlook.com>
2021-09-30 15:22:03 +0700

    edit save

--- 1.master_data/fn_SaveMasterBank.js
+++ 1.master_data/fn_SaveMasterBank.js
@@ -4,19 +4,25 @@ const saveLog = require("../fn_SaveLog")
 
 const saveMasterBank = (req, res) => {
     sql.connect(config, (err) => {
-        let request = new sql.Request()
-        request.input("bank_code", sql.NVarChar(5), req.body.bank_code)
-		request.input("bank_name", sql.NVarChar(50), req.body.bank_name)
-		request.input("address", sql.NVarChar(255), req.body.address)
-		request.input("email", sql.NVarChar(50), req.body.email)
-		request.input("user_name", sql.NVarChar(20), req.body.user_name)
-		request.execute("sp_save_master_bank", (err, result) => {
+        try {
+            let request = new sql.Request()
+            request.input("bank_code", sql.NVarChar(5), req.body.bank_code)
+            request.input("bank_name", sql.NVarChar(50), req.body.bank_name)
+            request.input("address", sql.NVarChar(255), req.body.address)
+            request.input("email", sql.NVarChar(50), req.body.email)
+            request.input("user_name", sql.NVarChar(20), req.body.user_name)
+            request.execute("sp_save_master_bank", (err, result) => {
             if(err){
                 saveLog("saveMasterBank", "error", "request body", err.originalError.message, null, req.body.user_name, req.body.ip_address)
                 return res.status(501).json({ message: "error", description: err.originalError.message })
             }
-            res.status(200).json(result.recordset[0])
+            return res.status(200).json(result.recordset)
         })
+        } catch (err) {
+            saveLog("saveMasterBank", "error", "request sql execute", err.originalError.message, null, req.body.user_name, req.body.ip_address)
+            return res.status(501).json({ message: "error", description: err.originalError.message })
+        }
+        
     })
 }
 
--- 1.master_data/fn_SaveMasterUser.js
+++ 1.master_data/fn_SaveMasterUser.js
@@ -4,21 +4,27 @@ const saveLog = require("../fn_SaveLog")
 
 const saveMasterUser = (req, res) => {
     sql.connect(config, () => {
-        let request = new sql.Request()
-        request.input("user_name", sql.NVarChar(20), req.body.user_name)
-		request.input("first_name", sql.NVarChar(50), req.body.first_name)
-		request.input("last_name", sql.NVarChar(50), req.body.last_name)
-		request.input("personal_id", sql.NVarChar(20), req.body.personal_id)
-		request.input("department_code", sql.NVarChar(5), req.body.department_code)
-        request.input("userlevel_id", sql.Int, req.body.userlevel_id)
-		request.input("create_by", sql.NVarChar(20), req.body.create_by)
-		request.execute("sp_save_master_user", (err, result) => {
-            if(err){
-                saveLog("Save new user", "error", "request body", err.originalError.message, null, req.body.create_by, req.body.ip_address)
-                res.status(501).json({ message: "error", description: err.originalError.message })
-            }
-            res.status(200).json(result.recordset)
+        try {
+            let request = new sql.Request()
+            request.input("user_name", sql.NVarChar(20), req.body.user_name)
+            request.input("first_name", sql.NVarChar(50), req.body.first_name)
+            request.input("last_name", sql.NVarChar(50), req.body.last_name)
+            request.input("personal_id", sql.NVarChar(20), req.body.personal_id)
+            request.input("department_code", sql.NVarChar(5), req.body.department_code)
+            request.input("userlevel_id", sql.Int, req.body.userlevel_id)
+            request.input("create_by", sql.NVarChar(20), req.body.create_by)
+            request.execute("sp_save_master_user", (err, result) => {
+                if(err){
+                    saveLog("saveMasterUser", "error", "request body", err.originalError.message, null, req.body.user_name, req.body.ip_address)
+                    return res.status(500).json({ message: "error", description: err.originalError.message })
+                }
+            return res.status(200).json(result.recordset)
         })
+        } catch (err) {
+            saveLog("Save new user", "error", "request body", err.originalError.message, null, req.body.create_by, req.body.ip_address)
+            return res.status(501).json({ message: "error", description: err.originalError.message })
+        }
+        
     })
 }
 
