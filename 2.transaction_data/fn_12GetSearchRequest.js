97de84d3901b0d13b1c3338d61003b4a84526171
Rathapong <dev01tcm@outlook.com>
2021-10-04 17:34:14 +0700

    edit get request

--- 2.transaction_data/fn_12GetSearchRequest.js
+++ 2.transaction_data/fn_12GetSearchRequest.js
@@ -6,16 +6,15 @@ const searchRequest = (req, res) => {
     sql.connect(config, () => {
         try {
             let request = new sql.Request()
-			request.input("request_code", sql.NVarChar(10), req.body.request_code)
-			request.input("from_date", sql.NVarChar(20), req.body.from_date)
-            request.input("to_date", sql.NVarChar(20), req.body.to_date)
-			request.input("request_status", sql.Int, req.body.request_status)
-			request.execute("sp_get_request", (err, result) => {
-                console.log(result)
-                return res.status(200).json(result.recordset)
+			request.input("request_code", sql.NVarChar(10), req.query.request_code)
+            request.input("employer_account", sql.NVarChar(10), req.query.employer_account)
+            request.input("refference_id", sql.NVarChar(20), req.query.refference_id)
+			request.execute("sp_get_trans_request", (err, result) => {
+                res.status(200).json(result.recordset)
             })
         } catch (err) {
-            console.log(err)
+            saveLog("get request", "error", "execute store", err.originalError, null, req.body.user_name, req.body.ip_address)
+            return res.status(400).json({ message: "error", description: err.originalError.message })
         }
     })
 }
