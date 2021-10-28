9161befa1a9a62e139e569f82619f4be75074955
Rathapong <dev01tcm@outlook.com>
2021-10-28 09:59:56 +0700

    edit download file

--- 2.transaction_data/fn_DownloadAttachFile.js
+++ 2.transaction_data/fn_DownloadAttachFile.js
@@ -1,6 +1,18 @@
+const fs = require('fs');
+
 const downloadAttachFile = async (req, res) => {
 	const file_name = req.query.file_name
 	const file = `./public/attach/${file_name}`
 	res.download(file)
 }
-module.exports = downloadAttachFile
+
+const downloadAttachFile2 = async(req, res) => {
+	const dir = `./public/attach/`
+	fs.readdirSync(dir).forEach(files => {
+		if(req.query.file_name === files.split('.').slice(0, -1).join('.')){
+			const file = `./public/attach/${files}`
+			res.download(file)
+		}
+	});
+}
+module.exports = downloadAttachFile2
